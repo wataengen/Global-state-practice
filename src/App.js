23a956b572b7d480e8469492201aaae6862f473e
Wataru Nakamura <130727655+wataengen@users.noreply.github.com>
2023-10-13 22:58:00 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Router } from "./router/Router";
+import "./styles.css";
+import { UserProvider } from "./providers/UserProvider";
+
+export default function App() {
+  return (
+    <UserProvider>
+      <Router />
+    </UserProvider>
+  );
+}
